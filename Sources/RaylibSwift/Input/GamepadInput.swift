8c8b32462aa10bc78399e11ad7eb2f7e1e205eb7
Myrddin Krustowski <54777517+theundergroundsorcerer@users.noreply.github.com>
2025-03-10 15:57:27 +0200

    removed rebudant comments in GamepadInput.swift

--- Sources/RaylibSwift/Input/GamepadInput.swift
+++ Sources/RaylibSwift/Input/GamepadInput.swift
@@ -184,18 +184,4 @@ extension Input {
 
 public typealias GamepadButton = Input.Gamepad.Button
 public typealias GamepadAxis = Input.Gamepad.Axis
-
-/*
-// Input-related functions: gamepads
-RLAPI bool IsGamepadAvailable(int gamepad);                   // Check if a gamepad is available
-RLAPI const char *GetGamepadName(int gamepad);                // Get gamepad internal name id
-RLAPI bool IsGamepadButtonPressed(int gamepad, int button);   // Check if a gamepad button has been pressed once
-RLAPI bool IsGamepadButtonDown(int gamepad, int button);      // Check if a gamepad button is being pressed
-RLAPI bool IsGamepadButtonReleased(int gamepad, int button);  // Check if a gamepad button has been released once
-RLAPI bool IsGamepadButtonUp(int gamepad, int button);        // Check if a gamepad button is NOT being pressed
-RLAPI int GetGamepadButtonPressed(void);                      // Get the last gamepad button pressed
-RLAPI int GetGamepadAxisCount(int gamepad);                   // Get gamepad axis count for a gamepad
-RLAPI float GetGamepadAxisMovement(int gamepad, int axis);    // Get axis movement value for a gamepad axis
-RLAPI int SetGamepadMappings(const char *mappings);           // Set internal gamepad mappings (SDL_GameControllerDB)
-RLAPI void SetGamepadVibration(int gamepad, float leftMotor, float rightMotor, float duration); // Set gamepad vibration for both motors (duration in seconds)
-*/
+public typealias Gamepad = Input.Gamepad
