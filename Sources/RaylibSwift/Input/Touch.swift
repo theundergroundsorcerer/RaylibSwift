e2fde06cf11dffaa361849afce584c65f2fda932
Myrddin Krustowski <54777517+theundergroundsorcerer@users.noreply.github.com>
2025-03-16 16:48:33 +0200

    Made touch functionability functions more concise

--- Sources/RaylibSwift/Input/Touch.swift
+++ Sources/RaylibSwift/Input/Touch.swift
@@ -3,31 +3,31 @@ import CRaylib
 public enum Touch {
     /// Get touch position X for touch point 0 (relative to screen size)
     @inlinable 
-    public static var touchX: Int32 {
+    public static var x: Int32 {
         CRaylib.GetTouchX()
     }
 
     /// Get touch position Y for touch point 0 (relative to screen size)
     @inlinable
-    public static var touchY: Int32 {
+    public static var y: Int32 {
         CRaylib.GetTouchY()
     }
 
     /// Get touch position XY for a touch point index (relative to screen size)
     @inlinable
-    public static func touchPosition(index: Int32) -> Vector2 {
+    public static func position(index: Int32) -> Vector2 {
         CRaylib.GetTouchPosition(index)
     }
 
     /// Get touch point identifier for given index
     @inlinable
-    public static func touchPointId(index: Int32) -> Int32 {
+    public static func pointId(index: Int32) -> Int32 {
         CRaylib.GetTouchPointId(index)
     }
 
     /// Get number of touch points
     @inlinable
-    public static var touchPointCount: Int32 {
+    public static var pointCount: Int32 {
         CRaylib.GetTouchPointCount()
     }
 }
