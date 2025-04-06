ca5f45c28c4c6372a0aac4061b56b109fc0b25e3
Myrddin Krustowski <54777517+theundergroundsorcerer@users.noreply.github.com>
2025-04-06 22:03:38 +0300

    Removed unnecessary pointer wrappers

--- Sources/RaylibSwift/Gui/Gui.swift
+++ Sources/RaylibSwift/Gui/Gui.swift
@@ -2,32 +2,18 @@ import CRaygui
 
 public enum Gui {
     /// Slider Bar control
-    @discardableResult
     public static func sliderBar(
         bounds: Rectangle,
         textLeft: Text,
         textRight: Text,
         value: inout Float,
         minValue: Float,
-        maxValue: Float
-    ) -> Int32 {
-        textLeft.withCString { leftTextPtr in
-            textRight.withCString { rightTextPtr in
-                withUnsafeMutablePointer(to: &value) { valuePtr in
-                    CRaygui.GuiSliderBar(
-                        bounds, leftTextPtr, rightTextPtr, valuePtr, minValue, maxValue)
-                }
-            }
+        maxValue: Float) -> Int32 {
+            CRaygui.GuiSliderBar(bounds, textLeft, textRight, &value, minValue, maxValue)
         }
-    }
-
+    
     /// Check Box control, returns true when active
-    @discardableResult
     public static func checkBox(bounds: Rectangle, text: Text, checked: inout Bool) -> Int32 {
-        text.withCString { textPtr in
-            withUnsafeMutablePointer(to: &checked) { checkedPtr in
-                CRaygui.GuiCheckBox(bounds, textPtr, checkedPtr)
-            }
-        }
+        GuiCheckBox(bounds, text, &checked)
     }
 }
