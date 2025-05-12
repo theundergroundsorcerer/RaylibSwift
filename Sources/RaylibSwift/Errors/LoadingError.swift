e9d2531da98be6f74e32a922e30822061e3b8c9f
Myrddin Krustowski <54777517+theundergroundsorcerer@users.noreply.github.com>
2025-05-12 15:26:21 +0300

    Added Game protocol and LoadingError. The approach is to move the code and structure to more modular and Swift idiomatic protocol and object oriented structure. Also, error will allow throwing in future versions.

---
+++ Sources/RaylibSwift/Errors/LoadingError.swift
@@ -0,0 +1,3 @@
+public struct LoadingError: Error {
+    var details: String
+}
---
+++ Sources/RaylibSwift/Game/Game.swift
@@ -0,0 +1,74 @@
+/// Game protocol for runnable Game apps
+public protocol Game: AnyObject {
+    /// Code that loads the necessary game data that could not be loaded during initialization 
+    /// goes here
+    func loadData() throws(LoadingError)
+
+    /// Creates game window and runs sets the neccessary raylib configuration flags
+    func beginGame()
+
+    /// runs the game loop at target fps
+    func loop(targetFPS: Int32)
+
+    /// handles keyboard, gamepad, mouse and touch input here
+    /// In general - the interactions with input devices should be done here
+    func handleInput()
+
+    /// update the game state
+    func update()
+    
+    /// displays the game state for a given frame
+    func displayFrame()
+
+    /// by default, it is just a call back to display frame. soemtimes, additional things 
+    /// not related to game state can be drawn
+    func draw()
+
+    /// cleans up the game and closes the window
+    func close()
+
+    /// creates and runs single instance of game
+    static func run()
+
+    /// creates an instance of game with default settings
+    static func make() -> Self
+}
+
+/// default implementations of some game function
+extension Game {
+    func loadData() throws(LoadingError) {}
+
+    func handleInput() {}
+
+    func loop(targetFPS: Int32) {
+        Time.setFps(targetFPS)
+        while !Window.shouldClose {
+            handleInput()
+            update()
+            draw()
+        }
+    }
+
+    func draw() {
+        Graphics.beginDrawing()
+        displayFrame()
+        Graphics.endDrawing()
+    }
+
+    func close() {
+        if Window.shouldClose { Window.close() }
+    }
+
+    static func run() {
+        let game = make()
+        do {
+            try game.loadData()
+            game.beginGame()
+            game.loop(targetFPS: 60)
+            game.close()
+        } catch let loadingError {
+            print(loadingError)
+            fatalError("Could not load game data")
+        }
+    }
+}
