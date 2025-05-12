90374c8ebd8176db590643a7673c3a62bd3e3e29
Myrddin Krustowski <54777517+theundergroundsorcerer@users.noreply.github.com>
2025-05-12 13:23:58 +0300

    Added more wrapper functions to Gui

--- Sources/RaylibSwift/Gui/Gui.swift
+++ Sources/RaylibSwift/Gui/Gui.swift
@@ -9,13 +9,43 @@ public enum Gui {
         textRight: Text,
         value: inout Float,
         minValue: Float,
-        maxValue: Float) -> Int32 {
-            CRaygui.GuiSliderBar(bounds, textLeft, textRight, &value, minValue, maxValue)
-        }
-    
+        maxValue: Float
+    ) -> Int32 {
+        CRaygui.GuiSliderBar(bounds, textLeft, textRight, &value, minValue, maxValue)
+    }
+
     /// Check Box control, returns true when active
     @discardableResult
     public static func checkBox(bounds: Rectangle, text: Text, checked: inout Bool) -> Int32 {
-        GuiCheckBox(bounds, text, &checked)
+        CRaygui.GuiCheckBox(bounds, text, &checked)
+    }
+
+    /// Label control
+    @discardableResult
+    public static func label(bounds: Rectangle, text: Text) -> Int32 {
+        CRaygui.GuiLabel(bounds, text)
+    }
+
+    /// Dropdown Box control
+    @discardableResult
+    public static func dropdownBox(
+        bounds: Rectangle,
+        text: Text,
+        active: inout Int32,
+        editMode: Bool
+    ) -> Int32 {
+        CRaygui.GuiDropdownBox(
+            bounds, text, &active, editMode)
+    }
+    /// int GuiDropdownBox(Rectangle bounds, const char *text, int *active, bool editMode);
+
+    /// Lock gui controls (global state)
+    public static func lock() {
+        CRaygui.GuiLock()
+    }
+
+    /// Unlock gui controls (global state)
+    public static func unlock() {
+        CRaygui.GuiUnlock()
     }
 }
